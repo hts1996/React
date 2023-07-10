a0d3f4d4e9f8609ce289ccdeb4427f2c6039ddd1
HT <tgx963@naver.com>
2023-07-10 23:11:14 +0900

    type

--- apps/src/routes/Detail.js
+++ apps/src/routes/Detail.js
@@ -1,12 +1,20 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 function Detail() {
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(json);
+      axios({
+        method:'get',
+        url:`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`,
+
+      }).then((response) => {
+        console.log(12423412)
+        console.log(response.data);
+        setMovie(response.data.results);
+        setLoading(false);
+      }))
   };
   useEffect(() => {
     getMovie();
